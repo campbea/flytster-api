2c1b705b4344422328748d2868e10d740c9c4f15
campbea <campbellandrewaof@yahoo.com>
2016-02-19 16:08:56 -0600

    credits initial commit

---
+++ flytster/credits/models.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.db import models
+
+
+def set_expiration():
+    return now() + timedelta(years=1)
+
+
+class Credit(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='credits')
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
+    used = model.BooleanField(default=False)
+    expiration = models.DateTimeField(default=set_expiration)
+    timestamp = models.DateTimeField(auto_now_add=True)
